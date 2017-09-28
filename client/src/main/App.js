439f0d612da3bafad1a541625e14e842f2bb3b0e
vinhnguyen82 <vinhnguyentx@utexas.edu>
2017-09-28 11:43:22 -0500

    add spec

--- client/src/main/App.js
+++ client/src/main/App.js
@@ -2,25 +2,26 @@ import React, {Component} from 'react';
 import '../App.css';
 
 class App extends Component {
-  constructor() {
-    super()
-      this.state = {
-        addSpaceFormActive: false
-      }
+    constructor() {
+        super()
+        this.state = {
+            addSpaceFormActive: false
+        }
+    }
 
-  }
+    handleAddSpaceForm = () => {
+        this.setState({
+            addSpaceFormActive: !this.state.addSpaceFormActive
+        })
+    }
 
-  handleAddSpace() {
-
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <button>Add Space</button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <button>Add Space</button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../main/App';
+import { shallow } from 'enzyme';
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
 });
 
 describe('handleAddSpace', () => {
-  it('updates the state of AddSpaceFormActiveToTrue when it is false', () => {
-      const div = document.createElement('div');
-      const app = ReactDOM.render(<App />, div);
+    let app
 
-      app.instance().handleAddSpaceForm()
-      expect(app.state().addSpaceFormActive).toEqual(true)
-  })
+    beforeAll(() => {
+        app = shallow(<App/>)
+    })
+
+
+    it('updates the state of AddSpaceFormActiveToTrue when it is false', () => {
+        app.instance().handleAddSpaceForm()
+
+        expect(app.state().addSpaceFormActive).toEqual(true)
+    })
 })
